92b47d5e12022f1d8050fd6d0c038f97bb33b61d
amesar <amesar@users.noreply.github.com>
2021-04-15 17:12:29 +0000

    Updated MLflow version in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(name="mlflow_export_import",
       version="1.0.0",
       author="Andre M",
-      description="MLflow Export/Import experiments, runs or models",
+      description="MLflow export/import experiments, runs or registered models",
       url="https://github.com/amesar/mlflow-export-import",
       python_requires=">=3.7",
       packages=find_packages(),
       zip_safe=False,
       install_requires=[
-          "mlflow>=1.12.1",
+          "mlflow>=1.15.0",
           "pytest==5.3.5",
           "wheel"
       ])
