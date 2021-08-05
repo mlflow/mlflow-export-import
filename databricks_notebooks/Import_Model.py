438db29e3f755264df9e2e1ee205f57655147f9f
Andre Mesarovic <amesar@users.noreply.github.com>
2021-08-05 12:51:40 -0400

    first commit

---
+++ databricks_notebooks/Import_Model.py
@@ -0,0 +1,73 @@
+# Databricks notebook source
+# MAGIC %md ### Import Model
+# MAGIC 
+# MAGIC ##### Overview
+# MAGIC 
+# MAGIC * Import a registered model from a folder.
+# MAGIC * See notebook [Export_Model]($Export_Model).
+# MAGIC 
+# MAGIC ##### Widgets
+# MAGIC * Model - new registered model name.
+# MAGIC * Experiment name - contains runs created for model versions.
+# MAGIC * Input folder - Input directory containing the exported model.
+# MAGIC 
+# MAGIC #### Limitations
+# MAGIC * There is a bug where you cannot create a model with the same name as a deleted model.
+# MAGIC 
+# MAGIC #### Setup
+# MAGIC * See Setup in [README]($00_README_Export_Import).
+
+# COMMAND ----------
+
+dbutils.widgets.text(" Model", "") 
+model_name = dbutils.widgets.get(" Model")
+
+dbutils.widgets.text("Experiment name", "") 
+experiment_name = dbutils.widgets.get("Experiment name")
+
+dbutils.widgets.text("Input folder", "") 
+input_dir = dbutils.widgets.get("Input folder")
+
+if len(input_dir)==0: raise Exception("ERROR: Input folder is required")
+import os
+os.environ["INPUT_DIR"] = input_dir.replace("dbfs:","/dbfs")
+
+model_name, input_dir, experiment_name
+
+# COMMAND ----------
+
+# MAGIC %md ### Display model files to be imported
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l $INPUT_DIR
+
+# COMMAND ----------
+
+# MAGIC %sh cat $INPUT_DIR/model.json
+
+# COMMAND ----------
+
+# MAGIC %md ### Import model
+
+# COMMAND ----------
+
+from mlflow_export_import.model.import_model import ModelImporter
+importer = ModelImporter()
+importer.import_model(input_dir, model_name, experiment_name, delete_model=True)
+
+# COMMAND ----------
+
+# MAGIC %md ### Display MLflow UI URIs
+
+# COMMAND ----------
+
+# MAGIC %run ./Common
+
+# COMMAND ----------
+
+display_registered_model_uri(model_name)
+
+# COMMAND ----------
+
+display_experiment_uri(experiment_name)
