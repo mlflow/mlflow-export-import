3f834273d6c8d921ed4470cf4183149825d37977
Siddharth Murching <smurching@gmail.com>
2022-01-04 10:34:18 -0500

    Update user_agent_header.py

--- mlflow_export_import/common/user_agent_header.py
+++ mlflow_export_import/common/user_agent_header.py
@@ -3,7 +3,6 @@ Set User-Agent header as 'mlflow-export-import/1.0.0'.
 """
 
 from mlflow.tracking.request_header.abstract_request_header_provider import RequestHeaderProvider
-from mlflow.tracking.request_header.registry import RequestHeaderProviderRegistry
 
 _user_agent = "mlflow-export-import/1.0.0"
 
@@ -14,7 +13,3 @@ class MlflowExportImportRequestHeaderProvider(RequestHeaderProvider):
         return True
     def request_headers(self):
         return { "User-Agent": _user_agent }
-
-_request_header_provider_registry = RequestHeaderProviderRegistry()
-_request_header_provider_registry.register(MlflowExportImportRequestHeaderProvider)
-_request_header_provider_registry.register_entrypoints()
