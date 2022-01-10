095e9f6cbfd7c5838edc35c5c507b9c04ae6c316
amesar <amesar@users.noreply.github.com>
2022-01-10 07:26:43 +0000

    JSON pretty print for list_registered_models.py

--- mlflow_export_import/model/list_registered_models.py
+++ mlflow_export_import/model/list_registered_models.py
@@ -3,6 +3,7 @@ Lists all registered models.
 """
 
 import os
+import json
 import click
 from mlflow_export_import.common.http_client import MlflowHttpClient
 
@@ -17,8 +18,9 @@ def main(output_dir):  # pragma: no cover
     rsp = client._get("registered-models/list")
     path = os.path.join(output_dir,"registered_models.json")
     print("Output file:",path)
+    dct = json.loads(rsp.text)
     with open(path, "w") as f:
-        f.write(rsp.text)
+        f.write(json.dumps(dct,indent=2)+"\n")
 
 if __name__ == "__main__":
     main()
