b6a5bb886b9f2047ee0edbd260ab2c17a33f2b57
amesar <amesar@users.noreply.github.com>
2022-06-22 16:21:11 +0000

    #14 - databricks_tester.py: refactor jobs_service.submit_run call into _run_job

--- tests/databricks/databricks_tester.py
+++ tests/databricks/databricks_tester.py
@@ -30,12 +30,13 @@ class DatabricksTester():
     def __init__(self, ws_base_dir, dst_base_dir, cluster_spec, model_name, run_name_prefix, profile=None, verbose=False):
         api_client = utils.get_api_client(profile)
         self.ws_api = WorkspaceApi(api_client)
-        self.jobs_service = service.JobsService(api_client)
         self.dbfs_api = DbfsApi(api_client)
         self.cluster_api = ClusterApi(api_client)
+        self.jobs_service = service.JobsService(api_client)
 
         self.ws_base_dir = ws_base_dir
         self.dst_base_dir = dst_base_dir
+        self.dst_run_base_dir = os.path.join(dst_base_dir,"runs")
         self.dst_exp_base_dir = os.path.join(dst_base_dir,"experiments")
         self.dst_model_base_dir = os.path.join(dst_base_dir,"models")
         self.model_name = model_name
@@ -46,13 +47,17 @@ class DatabricksTester():
 
         self.ml_nb_path = os.path.join(self.ws_base_dir, _experiment_nb)
         self.ml_exp_path = os.path.join(self.ws_base_dir, _experiment_name)
+        self._make_dirs()
 
+
+    def _make_dirs(self):
         self._ws_mkdir(self.ws_base_dir)
         self._ws_list(self.ws_base_dir)
         self._import_notebook(_fs_experiment_nb_name, "experiment", self.ws_base_dir) # import the model training notebook
-        self._import_notebooks(_fs_nb_base_dir, ws_base_dir) # import all the export-import notebooks
+        self._import_notebooks(_fs_nb_base_dir, self.ws_base_dir) # import all the export-import notebooks
 
-        self.dbfs_api.mkdirs(DbfsPath(dst_base_dir))
+        self.dbfs_api.mkdirs(DbfsPath(self.dst_base_dir))
+
 
     def _get_cluster_id(self):
         if isinstance(self.cluster_spec, str):
@@ -68,23 +73,26 @@ class DatabricksTester():
             self.cluster_api.permanent_delete(self.cluster_id)
 
 
+    def _run_job(self, nb_name, notebook_task):
+        run_name = self._mk_run_name(nb_name)
+        if self.verbose:
+            self._dump_json(f"Run job {run_name} - notebook_task", notebook_task)
+        return self.jobs_service.submit_run(run_name, existing_cluster_id=self.cluster_id, notebook_task=notebook_task)
+
+
     def run_export_experiment_job(self):
         nb_name = "Export_Experiment"
         nb_path = os.path.join(self.ws_base_dir, nb_name)
-        exp_name = os.path.join(self.ws_base_dir, _experiment_name)
         notebook_task = { 
             "notebook_path": nb_path,
             "base_parameters": {
-              " Experiment ID or Name": exp_name,
+              " Experiment ID or Name": self.ml_exp_path,
               "Destination base folder": self.dst_exp_base_dir,
-              "Export metadata tags": _export_src_tags,
+              "Export source tags": _export_src_tags,
               "Registered model": self.model_name
             }
         }
-        run_name = self._mk_run_name(nb_name)
-        if self.verbose:
-            self._dump_json("run_export_experiment_job - notebook_task", notebook_task)
-        return self.jobs_service.submit_run(run_name, existing_cluster_id=self.cluster_id, notebook_task=notebook_task)
+        return self._run_job(nb_name, notebook_task)
 
 
     def run_export_model_job(self):
@@ -95,11 +103,10 @@ class DatabricksTester():
             "base_parameters": {
               " Model": self.model_name,
               "Destination base folder": self.dst_model_base_dir,
-              "Export metadata tags": _export_src_tags,
+              "Export source tags": _export_src_tags,
             }
         }
-        run_name = self._mk_run_name(nb_name)
-        return self.jobs_service.submit_run(run_name, existing_cluster_id=self.cluster_id, notebook_task=notebook_task)
+        return self._run_job(nb_name, notebook_task)
 
 
     def run_training_job(self):
@@ -125,11 +132,13 @@ class DatabricksTester():
         run = workflow_client.get_run(run_id)
         self._dump_json("Run - final",run)
     
+
     def _import_notebook(self, nb_name, src_dir, dst_dir):
         src = os.path.join(src_dir, nb_name)
         dst = os.path.join(dst_dir, nb_name.replace(".py",""))
         self.ws_api.import_workspace(src, dst, "PYTHON", "SOURCE", True)
     
+
     def _import_notebooks(self, src_dir, dst_dir):
         self.ws_api.import_workspace_dir(src_dir, dst_dir, True, True)
     
@@ -137,14 +146,17 @@ class DatabricksTester():
     def _ws_mkdir(self, ws_base_dir):
         self.ws_api.mkdirs(ws_base_dir)
 
+
     def _mk_run_name(self, nb_name):
         return f"{self.run_name_prefix}_{nb_name}"
+
     
     def _ws_list(self, path):
         lst = self.ws_api.list_objects(path)
         print(f"Workspace objects for {path}:")
         for x in lst:
             print("  ",x.basename)
+
     
     def _dump_json(self, msg, dct):
         print(f"{msg}:")
