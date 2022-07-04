3caff88eb3912066f84b65ba3b8b79873e6a0aae
amesar <amesar@users.noreply.github.com>
2022-07-04 12:01:26 +0000

    bulk_utils.py: refactor of comon experiment ID and model name list logic into _get_list function

--- mlflow_export_import/bulk/bulk_utils.py
+++ mlflow_export_import/bulk/bulk_utils.py
@@ -1,35 +1,33 @@
 from mlflow_export_import.common.iterators import ListRegisteredModelsIterator
 from mlflow_export_import.common.iterators import ListExperimentsIterator
 
-def get_experiment_ids(client, experiment_ids):
-    """
-    Return a list experiment IDS
-    """
-    if isinstance(experiment_ids,str):
-        if experiment_ids == "all":
-            return [ exp.experiment_id for exp in ListExperimentsIterator(client) ]
-        elif experiment_ids.endswith("*"):
-            exp_prefix = experiment_ids[:-1]
-            return [ exp.experiment_id for exp in ListExperimentsIterator(client) if exp.name.startswith(exp_prefix) ] # Wish there was an experiment search method for efficiency
-        else:
-            return experiment_ids.split(",")
-    elif isinstance(experiment_ids,list):
-        return experiment_ids
-    else:
-        return experiment_ids
-        #raise MlflowExportImportException(f"Argument to get_experiment_ids() is of type '{type(experiment_ids)}. Must must be a string or list")
 
-def get_model_names(client, model_names):
-    if isinstance(model_names,str):
-        if model_names == "all":
-            model_names = [ model.name for model in ListRegisteredModelsIterator(client) ]
-        elif model_names.endswith("*"):
-            model_prefix = model_names[:-1]
-            model_names = [ model.name for model in ListRegisteredModelsIterator(client) if model.name.startswith(model_prefix) ] # TODO: change to search_registered_models
+def _get_list(names, func_list):
+    """
+    Returns a list of entities specified by the 'names' filter.
+    :param names: Filter of desired list of entities. Can be: "all", comma-delimited string, list of entities or trailing wildcard "*".
+    :param func_list: Function that lists the entities primary keys - for experiments it is experiment_id, for registered models it is model name.
+    :return: List of entities.
+    """
+    if isinstance(names, str):
+        if names == "all":
+            return func_list()
+        elif names.endswith("*"):
+            prefix = names[:-1]
+            return [ x for x in func_list() if x.startswith(prefix) ] 
         else:
-            model_names = model_names.split(",")
-    elif isinstance(model_names,list):
-        return model_names
+            return names.split(",")
     else:
-        return model_names
-    return model_names
+        return names
+
+
+def get_experiment_ids(mlflow_client, experiment_ids):
+    def list_entities():
+        return [ exp.experiment_id for exp in ListExperimentsIterator(mlflow_client) ]
+    return _get_list(experiment_ids, list_entities)
+
+
+def get_model_names(mlflow_client, model_names):
+    def list_entities():
+        return [ model.name for model in ListRegisteredModelsIterator(mlflow_client) ]
+    return _get_list(model_names, list_entities)
