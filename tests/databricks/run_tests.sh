0d28bc2e9ee87273872b999469bfc8f080d22d2f
amesar <amesar@users.noreply.github.co>
2022-11-10 22:48:43 -0500

    Added --junitxml to py.test scripts

--- tests/databricks/run_tests.sh
+++ tests/databricks/run_tests.sh
@@ -2,5 +2,4 @@
 export PYTHONPATH=.:../..:..
 export MLFLOW_TRACKING_URI=databricks
 
-time -p python -u -m py.test -s --junitxml=test_junit.xml `ls tes*.py` | 2>&1 tee run_tests.log
-
+time -p python -u -m pytest -s --junitxml=test_junit.xml `ls tes*.py` | 2>&1 tee run_tests.log
