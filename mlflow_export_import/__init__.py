a6f7ea4a08845dedb58c0599382efc99047c7ea2
amesar <amesar@users.noreply.github.co>
2022-12-03 02:57:45 -0500

    __init__.py: removed no longer used BaseCopier class

--- mlflow_export_import/__init__.py
+++ mlflow_export_import/__init__.py
@@ -1,25 +1,15 @@
-
 import os
 import json
 import mlflow
 
-class BaseCopier():
-    def __init__(self, src_client, dst_client):
-        self.src_client = src_client
-        self.dst_client = dst_client
-
-    def get_experiment(self, client, exp_name):
-        exp = client.get_experiment_by_name(exp_name)
-        if exp is None:
-            exp_id = client.create_experiment(exp_name)
-            exp = client.get_experiment(exp_id)
-        return exp
 
 def mk_dbfs_path(path):
     return path.replace("/dbfs","dbfs:")
 
+
 def mk_local_path(path):
     return path.replace("dbfs:","/dbfs")
+
 
 def peek_at_experiment(exp_dir):
     manifest_path = os.path.join(exp_dir,"manifest.json")
@@ -28,10 +18,12 @@ def peek_at_experiment(exp_dir):
     print("manifest path:",manifest_path)
     print(content)
 
+
 def  create_client(uri):
     return mlflow.tracking.MlflowClient() if uri is None else mlflow.tracking.MlflowClient(uri)
 
-# monkey patch mlflow.tracking.MlflowClient
+
+# monkey patch mlflow.tracking.MlflowClient to return tracking URI in __repr__
 
 def add_repr_to_MlflowClient():
     def custom_repr(self): 
