7f493f46ac21e93c060f47618bb191375c61ce3b
Andre Mesarovic <amesar@users.noreply.github.com>
2023-01-18 13:13:26 -0500

    Issue #67:  Added Export_Models notebook

---
+++ databricks_notebooks/bulk/Export_Models.py
@@ -0,0 +1,100 @@
+# Databricks notebook source
+# MAGIC %md ## Export Models
+# MAGIC 
+# MAGIC Export specified or all models.
+# MAGIC 
+# MAGIC See https://github.com/mlflow/mlflow-export-import/blob/master/README_collection.md#registered-models for description of widget options.
+# MAGIC 
+# MAGIC Widgets
+# MAGIC * Models - comma seperated registered model names to be exported. 'all' will export all models.
+# MAGIC * Output folder
+# MAGIC * Stages - comma seperated stages to be exported
+# MAGIC * Export all runs 
+# MAGIC * Notebook formats
+# MAGIC * Use threads
+
+# COMMAND ----------
+
+# MAGIC %sh pip install mlflow-export-import
+
+# COMMAND ----------
+
+dbutils.widgets.text("1. Models", "") 
+models = dbutils.widgets.get("1. Models")
+
+dbutils.widgets.text("2. Output base directory", "dbfs:/mnt/andre-work/exim/experiments") 
+output_dir = dbutils.widgets.get("2. Output base directory")
+output_dir = output_dir.replace("dbfs:","/dbfs")
+
+all_formats = [ "SOURCE", "DBC", "HTML", "JUPYTER" ]
+dbutils.widgets.multiselect("3. Notebook formats",all_formats[0],all_formats)
+notebook_formats = dbutils.widgets.get("3. Notebook formats")
+
+dbutils.widgets.dropdown("4. Export all runs","no",["yes","no"])
+export_all_runs = dbutils.widgets.get("4. Export all runs") == "yes"
+
+dbutils.widgets.multiselect("5. Stages", "Production", ["Production","Staging","Archived","None"])
+stages = dbutils.widgets.get("5. Stages")
+
+dbutils.widgets.dropdown("6. Use threads","no",["yes","no"])
+use_threads = dbutils.widgets.get("6. Use threads") == "yes"
+
+export_notebook_revision = False
+export_all_runs = False
+
+import os
+os.environ["OUTPUT_DIR"] = output_dir
+
+print("models:",models)
+print("output_dir:",output_dir)
+print("stages:",stages)
+print("notebook_formats:",notebook_formats)
+print("export_all_runs:",export_all_runs)
+print("use_threads:",use_threads)
+
+# COMMAND ----------
+
+if len(models)==0: raise Exception("ERROR: Models are required")
+if len(output_dir)==0: raise Exception("ERROR: Output base directory is required")
+
+# COMMAND ----------
+
+# MAGIC %md ### Export models
+
+# COMMAND ----------
+
+from mlflow_export_import.bulk.export_models import export_models
+import mlflow
+export_models(mlflow.client.MlflowClient(), 
+              model_names=models, 
+              output_dir=output_dir,
+              notebook_formats=notebook_formats,
+              stages=stages, 
+              export_all_runs=export_all_runs,
+              use_threads=use_threads)
+
+# COMMAND ----------
+
+# MAGIC %md ### Display exported files
+
+# COMMAND ----------
+
+# MAGIC %sh 
+# MAGIC echo $OUTPUT_DIR
+# MAGIC ls $OUTPUT_DIR
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l $OUTPUT_DIR/models
+
+# COMMAND ----------
+
+# MAGIC %sh cat $OUTPUT_DIR/manifest.json
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l $OUTPUT_DIR/experiments
+
+# COMMAND ----------
+
+# MAGIC %sh cat $OUTPUT_DIR/experiments/manifest.json
