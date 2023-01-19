763eb4ad62db314850dcbf835f937cb3129d5ee2
Andre Mesarovic <amesar@users.noreply.github.com>
2023-01-18 23:27:40 -0500

    Issue #67: Import_Experiments notebook: Change to use '%run ./Common'

--- databricks_notebooks/bulk/Import_Experiments.py
+++ databricks_notebooks/bulk/Import_Experiments.py
@@ -5,11 +5,11 @@
 # MAGIC * Input directory - directory of exported experiments
 # MAGIC * Use threads - use multi-threaded import
 # MAGIC 
-# MAGIC See https://github.com/mlflow/mlflow-export-import/blob/master/README_bulk.md#experiments.
+# MAGIC See https://github.com/mlflow/mlflow-export-import/blob/master/README_collection.md#Import-experiments.
 
 # COMMAND ----------
 
-# MAGIC %sh pip install mlflow-export-import
+# MAGIC %run ./Common
 
 # COMMAND ----------
 
@@ -24,14 +24,14 @@ print("input_dir:",input_dir)
 
 # COMMAND ----------
 
-if len(input_dir)==0: raise Exception("ERROR: Input directory is required")
+assert_widget(input_dir, "1. Input directory")
 
 # COMMAND ----------
 
 from mlflow_export_import.bulk.import_experiments import import_experiments
 import mlflow
 import_experiments(
-    client=mlflow.tracking.MlflowClient(),
+    client=mlflow.client.MlflowClient(),
     input_dir=input_dir, 
     use_src_user_id=False, 
     use_threads=use_threads)
