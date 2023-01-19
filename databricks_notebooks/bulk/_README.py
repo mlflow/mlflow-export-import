a6c1959637ca92f09979fcfc82b8539c689f03a4
Andre Mesarovic <amesar@users.noreply.github.com>
2023-01-18 23:16:47 -0500

    databricks_notebooks/bulk/_README updates

--- databricks_notebooks/bulk/_README.py
+++ databricks_notebooks/bulk/_README.py
@@ -4,6 +4,6 @@
 # MAGIC Notebooks:
 # MAGIC * [Export_Experiments]($Export_Experiments) - [Import_Experiments]($Import_Experiments)
 # MAGIC * [Export_Models]($Export_Models) - [Import_Models]($Import_Models)
-# MAGIC * [Export_All]($Export_All) - To import all use, [Import_Models]($Import_Models)
+# MAGIC * [Export_All]($Export_All) - To 'Import_All' use [Import_Models]($Import_Models)
 # MAGIC 
 # MAGIC Last update: 2023-01-18
