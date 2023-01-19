99b5731d3c7b7a462d77d7d778961ec3178d8b6d
Andre Mesarovic <amesar@users.noreply.github.com>
2023-01-19 10:00:16 -0500

    databricks_notebooks/single: major update of notebooks per upcoming pypi version 2.0

--- databricks_notebooks/single/Import_Experiment.py
+++ databricks_notebooks/single/Import_Experiment.py
@@ -4,7 +4,6 @@
 # MAGIC **Widgets**
 # MAGIC * Input directory - DBFS input directory containing an exported experiment.
 # MAGIC * Destination experiment name - Will create if it doesn't exist.
-# MAGIC * Just peek - Just display manifest.json and do not import the experiment.
 # MAGIC 
 # MAGIC #### Setup
 # MAGIC * See the Setup section in [README]($./_README).
@@ -21,35 +20,20 @@ experiment_name = dbutils.widgets.get("1. Destination experiment name")
 dbutils.widgets.text("2. Input directory", "") 
 input_dir = dbutils.widgets.get("2. Input directory")
 
-dbutils.widgets.dropdown("3. Just peek","no",["yes","no"])
-just_peek = dbutils.widgets.get("3. Just peek") == "yes"
-
 print("input_dir:",input_dir)
 print("experiment_name:",experiment_name)
-print("just_peek:",just_peek)
 
 # COMMAND ----------
 
-if len(input_dir)==0: raise Exception("ERROR: Input directory is required")
-if len(experiment_name)==0: raise Exception("ERROR: Destination experiment name is required")
+assert_widget(experiment_name, "1. Destination experiment name")
+assert_widget(input_dir, "2. Input directory")
 
 # COMMAND ----------
 
 from mlflow_export_import.experiment.import_experiment import ExperimentImporter
-from mlflow_export_import import peek_at_experiment
 
-importer = ExperimentImporter(mlflow.tracking.MlflowClient())
-peek_at_experiment(input_dir)
-
-# COMMAND ----------
-
-input_dir
-
-# COMMAND ----------
-
- if not just_peek:
-    if len(experiment_name)==0: raise Exception("ERROR: Destination Experiment Name is required")
-    importer.import_experiment(experiment_name, input_dir)
+importer = ExperimentImporter(mlflow.client.MlflowClient())
+importer.import_experiment(experiment_name, input_dir)
 
 # COMMAND ----------
 
