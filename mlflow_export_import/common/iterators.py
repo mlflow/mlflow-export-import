9800fb494f7a6c151552937afc14c6c291b45116
amesar <amesar@users.noreply.github.co>
2023-03-06 01:00:05 -0500

    Issue #84: removed hard-coded max_results for search methods in search iterators

--- mlflow_export_import/common/iterators.py
+++ mlflow_export_import/common/iterators.py
@@ -1,29 +1,27 @@
 from mlflow.entities import ViewType
 import mlflow
 
-MAX_RESULTS = 500
-
 
 class BaseIterator():
     """
     Base class to iterate for list methods that return PageList.
     """
-
-    def _call_iter(self):
-        if mlflow.__version__ < "2.2.1": 
-            return self.method(self.filter)  #7623 - https://mlflow.org/docs/2.1.1/python_api/mlflow.client.html
-        else:
-            return self.method(self.filter, max_results=self.max_results) # https://mlflow.org/docs/latest/python_api/mlflow.client.html
-
-    def _call_next(self):
-        return self.method(self.filter, max_results=self.max_results, page_token=self.paged_list.token)
-
-    def __init__(self, method, max_results=MAX_RESULTS, filter=None):
-        self.method = method
+    def __init__(self, search_method, max_results=None, filter=None):
+        self.search_method = search_method
         self.filter = filter
         self.max_results = max_results
         self.idx = 0
         self.paged_list = None
+        self.kwargs = { "max_results": self.max_results } if self.max_results else {}
+
+    def _call_iter(self):
+        if mlflow.__version__ < "2.2.1":
+            return self.search_method(self.filter)  #7623 - https://mlflow.org/docs/2.1.1/python_api/mlflow.client.html
+        else:
+            return self.search_method(self.filter, **self.kwargs) # https://mlflow.org/docs/latest/python_api/mlflow.client.html
+
+    def _call_next(self):
+        return self.search_method(self.filter, page_token=self.paged_list.token, **self.kwargs)
 
     def __iter__(self):
         self.paged_list = self._call_iter()
@@ -51,8 +49,7 @@ class SearchExperimentsIterator(BaseIterator):
         for experiment in experiments:
             print(experiment)
     """
-
-    def __init__(self, client, view_type=ViewType.ACTIVE_ONLY, max_results=MAX_RESULTS, filter=None):
+    def __init__(self, client, view_type=ViewType.ACTIVE_ONLY, max_results=None, filter=None):
         super().__init__(client.search_experiments, max_results, filter)
         self.view_type = view_type
 
@@ -64,8 +61,8 @@ class SearchRegisteredModelsIterator(BaseIterator):
         for model in models:
             print(model)
     """
-    def __init__(self, client, max_results=MAX_RESULTS, filter=None):
-        super().__init__(client.search_registered_models, max_results, filter)
+    def __init__(self, client, max_results=None, filter=None):
+        super().__init__(client.search_registered_models, max_results=max_results, filter=filter)
 
 
 class SearchModelVersionsIterator(BaseIterator):
@@ -75,19 +72,18 @@ class SearchModelVersionsIterator(BaseIterator):
         for vr in versions:
             print(vr)
     """
-    def __init__(self, client, max_results=MAX_RESULTS, filter=None):
+    def __init__(self, client, max_results=None, filter=None):
         super().__init__(client.search_model_versions, max_results=max_results, filter=filter)
 
 
 class SearchRunsIterator(BaseIterator):
-    def __init__(self, client, experiment_id, max_results=MAX_RESULTS, filter=None):
+    def __init__(self, client, experiment_ids, max_results=None, filter=None):
         super().__init__(None, max_results, filter)
-        self.experiment_id = experiment_id
+        self.experiment_ids = experiment_ids
         self.client = client
 
     def _call_iter(self):
-        return self.client.search_runs(self.experiment_id, filter_string=self.filter, max_results=self.max_results)
+        return self.client.search_runs(self.experiment_ids, filter_string=self.filter, max_results=self.max_results)
 
     def _call_next(self):
-        return self.client.search_runs(self.experiment_id, filter_string=self.filter, max_results=self.max_results, page_token=self.paged_list.token)
-
+        return self.client.search_runs(self.experiment_ids, filter_string=self.filter, max_results=self.max_results, page_token=self.paged_list.token)
