365a2676b0fcf645c68973b927b6a72d5ff652ff
amesar <amesar@users.noreply.github.co>
2023-03-08 08:09:59 -0500

    databricks_notebooks/single: adjusted clashing _README files

--- databricks_notebooks/bulk/_README.py
+++ databricks_notebooks/bulk/_README.py
@@ -1,9 +1,24 @@
 # Databricks notebook source
-# MAGIC %md ## MLflow Export Import bulk notebooks
+# MAGIC %md ## MLflow Export Import - Bulk Notebooks
 # MAGIC 
-# MAGIC Notebooks:
-# MAGIC * [Export_Experiments]($Export_Experiments) - [Import_Experiments]($Import_Experiments)
-# MAGIC * [Export_Models]($Export_Models) - [Import_Models]($Import_Models)
-# MAGIC * [Export_All]($Export_All) - To 'Import_All' use [Import_Models]($Import_Models)
+# MAGIC Copy multiple MLflow objects. The target object name will be the same as the source object.
 # MAGIC 
-# MAGIC Last update: 2023-01-18
+# MAGIC **Notebooks**
+# MAGIC 
+# MAGIC * Experiments
+# MAGIC   * [Export_Experiments]($Export_Experiments) - export experiments and all their runs.
+# MAGIC   * [Import_Experiments]($Import_Experiments)
+# MAGIC * Registered Models
+# MAGIC   * [Export_Models]($Export_Models) - export models, their version runs and the experiments that the runs belong to.
+# MAGIC   * [Import_Models]($Import_Models)
+# MAGIC * All
+# MAGIC   * [Export_All]($Export_All) - export all MLflow objects. Note this can be expensive as there can be many MLflow API calls.
+# MAGIC   * Import_All - use [Import_Models]($Import_Models).
+# MAGIC 
+# MAGIC * [Common]($./Common)
+# MAGIC 
+# MAGIC Core code: https://github.com/mlflow/mlflow-export-import/blob/master/README_bulk.md.
+
+# COMMAND ----------
+
+# MAGIC %md Last updated: 2023-03-07
