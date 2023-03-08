458be78af05012579eaafaf80d9cf7b28f70acec
Andre Mesarovic <amesar@users.noreply.github.com>
2023-03-08 07:06:52 -0500

    Notebook single/Export_Model - issue-90-named-args

--- databricks_notebooks/single/Import_Model.py
+++ databricks_notebooks/single/Import_Model.py
@@ -1,22 +1,37 @@
 # Databricks notebook source
-# MAGIC %md ### Import Registered Model
+# MAGIC %md ### Export Registered Model
 # MAGIC 
 # MAGIC ##### Overview
+# MAGIC * Export a registered model and all the runs associated with its latest versions to a DBFS folder.
+# MAGIC * Output file `model.json` contains model metadata.
+# MAGIC * Each run and its artifacts are stored as a sub-directory.
 # MAGIC 
-# MAGIC * Import a registered model from a folder.
-# MAGIC * See notebook [Export_Model]($Export_Model).
+# MAGIC #### Output folder structure
+# MAGIC 
+# MAGIC ```
+# MAGIC +-model.json
+# MAGIC +-d2309e6c74dc4679b576a37abf6b6af8/
+# MAGIC | +-run.json
+# MAGIC | +-artifacts/
+# MAGIC |   +-plot.png
+# MAGIC |   +-sklearn-model/
+# MAGIC |   | +-model.pkl
+# MAGIC |   | +-conda.yaml
+# MAGIC |   | +-MLmodel
+# MAGIC ```
 # MAGIC 
 # MAGIC ##### Widgets
-# MAGIC * Model - new registered model name.
-# MAGIC * Experiment name - contains runs created for model versions.
-# MAGIC * Input directory - Input directory containing the exported model.
-# MAGIC * Delete model - delete model and its versions before importing.
-# MAGIC 
-# MAGIC #### Limitations
-# MAGIC * There is a bug where you cannot create a model with the same name as a deleted model.
+# MAGIC * Model - Registered model name.
+# MAGIC * Output base directory - Base output directory to which the model name will be appended to.
+# MAGIC * Notebook formats to export.
+# MAGIC * Stages to export.
 # MAGIC 
 # MAGIC #### Setup
 # MAGIC * See Setup in [README]($./_README).
+
+# COMMAND ----------
+
+# MAGIC %md ### Setup
 
 # COMMAND ----------
 
@@ -24,60 +39,41 @@
 
 # COMMAND ----------
 
-dbutils.widgets.text("1. Model name", "") 
-model_name = dbutils.widgets.get("1. Model name")
+dbutils.widgets.text("1. Model", "") 
+model_name = dbutils.widgets.get("1. Model")
 
-dbutils.widgets.text("2. Destination experiment name", "") 
-experiment_name = dbutils.widgets.get("2. Destination experiment name")
+dbutils.widgets.text("2. Output base directory", "") 
+output_dir = dbutils.widgets.get("2. Output base directory")
 
-dbutils.widgets.text("3. Input directory", "") 
-input_dir = dbutils.widgets.get("3. Input directory")
+notebook_formats = get_notebook_formats(3)
 
-dbutils.widgets.dropdown("4. Delete model","no",["yes","no"])
-delete_model = dbutils.widgets.get("4. Delete model") == "yes"
-
-import os
-os.environ["INPUT_DIR"] = input_dir.replace("dbfs:","/dbfs")
+all_stages = [ "All", "Production", "Staging", "Archived", "None" ]
+dbutils.widgets.multiselect("4. Stages", all_stages[0], all_stages)
+stages = dbutils.widgets.get("4. Stages")
+if stages == "All": 
+    stages = None
+else:
+    stages = stages.split(",")
+    if "" in stages: stages.remove("")
 
 print("model_name:", model_name)
-print("input_dir:", input_dir)
-print("experiment_name:", experiment_name)
-print("delete_model:", delete_model)
+print("output_dir:", output_dir)
+print("notebook_formats:", notebook_formats)
+print("stages:", stages)
 
 # COMMAND ----------
 
-assert_widget(model_name, "1. Model name")
-assert_widget(experiment_name, "2. Destination experiment name")
-assert_widget(input_dir, "3. Input directory")
+assert_widget(model_name, "1. Model")
+assert_widget(output_dir, "2. Output base directory")
+
+output_dir += f"/{model_name}"
+
+import mlflow
+print("output_dir:", output_dir)
 
 # COMMAND ----------
 
-# MAGIC %md ### Display model files to be imported
-
-# COMMAND ----------
-
-# MAGIC %sh ls -l $INPUT_DIR
-
-# COMMAND ----------
-
-# MAGIC %sh cat $INPUT_DIR/model.json
-
-# COMMAND ----------
-
-# MAGIC %md ### Import model
-
-# COMMAND ----------
-
-from mlflow_export_import.model.import_model import ModelImporter
-importer = ModelImporter(mlflow.client.MlflowClient())
-importer.import_model(model_name, 
-                      input_dir = input_dir, 
-                      experiment_name = experiment_name, 
-                      delete_model = delete_model)
-
-# COMMAND ----------
-
-# MAGIC %md ### Display MLflow UI URIs
+# MAGIC %md ### Display MLflow UI URI of Registered Model
 
 # COMMAND ----------
 
@@ -85,4 +81,39 @@ display_registered_model_uri(model_name)
 
 # COMMAND ----------
 
-display_experiment_uri(experiment_name)
+# MAGIC %md ### Export the model
+
+# COMMAND ----------
+
+from mlflow_export_import.model.export_model import ModelExporter
+exporter = ModelExporter(
+    mlflow.client.MlflowClient(),
+    notebook_formats = notebook_formats, 
+    stages = stages
+)
+exporter.export_model(
+    model_name = model_name, 
+    output_dir = output_dir
+)
+
+# COMMAND ----------
+
+# MAGIC %md ### Display  exported model files
+
+# COMMAND ----------
+
+import os
+output_dir = output_dir.replace("dbfs:","/dbfs")
+os.environ['OUTPUT_DIR'] = output_dir
+
+# COMMAND ----------
+
+# MAGIC %sh echo $OUTPUT_DIR
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l $OUTPUT_DIR
+
+# COMMAND ----------
+
+# MAGIC %sh cat $OUTPUT_DIR/model.json
