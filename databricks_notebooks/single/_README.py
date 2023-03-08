8f827992685f0af7f4ffabc434108f65f5bcd8f0
amesar <amesar@users.noreply.github.co>
2023-03-08 08:00:49 -0500

    databricks_notebooks/single: adjusted clashing _README files

--- databricks_notebooks/single/_README.py
+++ databricks_notebooks/single/_README.py
@@ -8,10 +8,10 @@
 # MAGIC   * [Export_Run]($./Export_Run)  
 # MAGIC   * [Import_Run]($./Import_Run)
 # MAGIC * Experiment
-# MAGIC   * [Export_Experiment]($./Export_Experiment) - export an experiment and all its runs (run.info, run.data and artifacts).
+# MAGIC   * [Export_Experiment]($./Export_Experiment) - export an experiment and its runs (run.info, run.data and artifacts).
 # MAGIC   * [Import_Experiment]($./Import_Experiment)
 # MAGIC * Registered Model
-# MAGIC   * [Export_Model]($./Export_Model) - export a model, its versions and the version's run.
+# MAGIC   * [Export_Model]($./Export_Model) - export a model, its versions and their runs.
 # MAGIC   * [Import_Model]($./Import_Model)
 # MAGIC * [Common]($./Common)
 # MAGIC 
@@ -19,4 +19,4 @@
 
 # COMMAND ----------
 
-# MAGIC %md Last updated: 2023-01-19
+# MAGIC %md Last updated: 2023-03-07
