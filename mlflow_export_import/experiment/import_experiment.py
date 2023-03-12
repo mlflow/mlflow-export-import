2bc37e8d171c5d57335eda383c6f1c9b82dd1c2d
amesar <amesar@users.noreply.github.co>
2023-03-12 17:24:20 -0400

    Isse #92: added entry point methods to experiment exporter/importer classes

--- mlflow_export_import/experiment/import_experiment.py
+++ mlflow_export_import/experiment/import_experiment.py
@@ -6,23 +6,56 @@ import os
 import click
 import mlflow
 
-from mlflow_export_import.common.click_options import opt_experiment_name, opt_input_dir, opt_import_source_tags, \
-    opt_use_src_user_id, opt_dst_notebook_dir
+from mlflow_export_import.common.click_options import (
+    opt_experiment_name,
+    opt_input_dir,
+    opt_import_source_tags,
+    opt_use_src_user_id,
+    opt_dst_notebook_dir
+)
+from mlflow_export_import.client.http_client import DatabricksHttpClient
 from mlflow_export_import.common import utils
 from mlflow_export_import.common import io_utils
 from mlflow_export_import.common import mlflow_utils
-from mlflow_export_import.client.http_client import DatabricksHttpClient
-from mlflow_export_import.run.import_run import RunImporter
+from mlflow_export_import.run.import_run import import_run
 from mlflow_export_import.common.source_tags import set_source_tags_for_field, mk_source_tags_mlflow_tag, fmt_timestamps
+
+
+def import_experiment(
+        experiment_name,
+        input_dir,
+        import_source_tags = False,
+        use_src_user_id = False,
+        dst_notebook_dir = None,
+        mlflow_client = None
+    ):
+    """
+    :param: experiment_name: Destination experiment name.
+    :param: input_dir: Source experiment directory.
+    :param import_source_tags: Import source information for MLFlow objects and create tags in destination object.
+    :param use_src_user_id: Set the destination user ID to the source user ID.
+                            Source user ID is ignored when importing into
+    :param mlflow_client: MLflow client.
+    :return: A map of source run IDs and destination run.info.
+    """
+    importer = ExperimentImporter(
+        import_source_tags = import_source_tags,
+        use_src_user_id = use_src_user_id,
+        mlflow_client = mlflow_client
+    )
+    return importer.import_experiment(
+        experiment_name = experiment_name,
+        input_dir = input_dir,
+        dst_notebook_dir = dst_notebook_dir
+    )
 
 
 class ExperimentImporter():
 
     def __init__(self, 
-            mlflow_client, 
             import_source_tags = False, 
-            mlmodel_fix = True, 
-            use_src_user_id = False
+            use_src_user_id = False,
+            mlflow_client = None, 
         ):
         """
         :param mlflow_client: MLflow client.
@@ -30,15 +63,10 @@ class ExperimentImporter():
         :param use_src_user_id: Set the destination user ID to the source user ID.
                                 Source user ID is ignored when importing into
         """
-        self.mlflow_client = mlflow_client
-        self.run_importer = RunImporter(self.mlflow_client, 
-            import_source_tags=import_source_tags,
-            mlmodel_fix=mlmodel_fix,
-            use_src_user_id=use_src_user_id,
-            dst_notebook_dir_add_run_id=True)
-        print("MLflowClient:", self.mlflow_client)
+        self.mlflow_client = mlflow_client or mlflow.client.MlflowClient()
         self.dbx_client = DatabricksHttpClient()
         self.import_source_tags = import_source_tags
+        self.use_src_user_id = use_src_user_id
 
 
     def import_experiment(self, 
@@ -74,7 +102,14 @@ class ExperimentImporter():
         run_ids_map = {}
         run_info_map = {}
         for src_run_id in run_ids:
-            dst_run, src_parent_run_id = self.run_importer.import_run(experiment_name, os.path.join(input_dir, src_run_id), dst_notebook_dir)
+            dst_run, src_parent_run_id = import_run(
+                mlflow_client = self.mlflow_client,
+                experiment_name = experiment_name, 
+                input_dir = os.path.join(input_dir, src_run_id), 
+                dst_notebook_dir = dst_notebook_dir,
+                import_source_tags = self.import_source_tags,
+                use_src_user_id = self.use_src_user_id
+            )
             dst_run_id = dst_run.info.run_id
             run_ids_map[src_run_id] = { "dst_run_id": dst_run_id, "src_parent_run_id": src_parent_run_id }
             run_info_map[src_run_id] = dst_run.info
@@ -82,6 +117,7 @@ class ExperimentImporter():
         if len(failed_run_ids) > 0:
             print(f"Warning: {len(failed_run_ids)} failed runs were not imported - see '{path}'")
         utils.nested_tags(self.mlflow_client, run_ids_map)
+
         return run_info_map
 
 
@@ -96,14 +132,11 @@ def main(input_dir, experiment_name, import_source_tags, use_src_user_id, dst_no
     print("Options:")
     for k,v in locals().items():
         print(f"  {k}: {v}")
-    importer = ExperimentImporter(
-        mlflow_client = mlflow.client.MlflowClient(),
+    import_experiment(
+        experiment_name = experiment_name,
+        input_dir = input_dir,
         import_source_tags = import_source_tags,
-        use_src_user_id = use_src_user_id
-    )
-    importer.import_experiment(
-        experiment_name = experiment_name, 
-        input_dir = input_dir, 
+        use_src_user_id = use_src_user_id,
         dst_notebook_dir = dst_notebook_dir
     )
 
--- tests/open_source/test_experiments.py
+++ tests/open_source/test_experiments.py
@@ -1,27 +1,35 @@
-from mlflow_export_import.experiment.export_experiment import ExperimentExporter
-from mlflow_export_import.experiment.import_experiment import ExperimentImporter
+from mlflow_export_import.experiment.export_experiment import export_experiment
+from mlflow_export_import.experiment.import_experiment import import_experiment
 from oss_utils_test import create_simple_run, init_output_dirs, create_dst_experiment_name
 from compare_utils import compare_runs, compare_experiment_tags
-from compare_utils import dump_runs
 from init_tests import mlflow_context
-
-_use_source_tags = True
 
 # == Setup
 
-def _init_exp_test(mlflow_context, exporter, importer, verbose=False):
+def _init_exp_test(mlflow_context, import_source_tags=False):
     init_output_dirs(mlflow_context.output_dir)
     exp1, run1 = create_simple_run(mlflow_context.client_src)
     run1 = mlflow_context.client_src.get_run(run1.info.run_id)
-    exporter.export_experiment(exp1.name, mlflow_context.output_dir)
 
-    experiment_name = create_dst_experiment_name(exp1.name)
-    importer.import_experiment(experiment_name, mlflow_context.output_dir)
-    exp2 = mlflow_context.client_dst.get_experiment_by_name(experiment_name)
+    export_experiment(
+        mlflow_client = mlflow_context.client_src,
+        experiment_id_or_name = exp1.name,
+        output_dir = mlflow_context.output_dir
+    )
+
+    dst_exp_name = create_dst_experiment_name(exp1.name)
+
+    import_experiment(
+        mlflow_client = mlflow_context.client_dst,
+        experiment_name = dst_exp_name,
+        input_dir = mlflow_context.output_dir,
+        import_source_tags = import_source_tags
+    )
+
+    exp2 = mlflow_context.client_dst.get_experiment_by_name(dst_exp_name)
     runs = mlflow_context.client_dst.search_runs(exp2.experiment_id)
     run2 = mlflow_context.client_dst.get_run(runs[0].info.run_id)
 
-    if verbose: dump_runs(run1, run2)
     return exp1, exp2, run1, run2
 
 
@@ -33,21 +41,17 @@ def _compare_experiments(exp1, exp2, import_source_tags=False):
 
 
 def test_exp_basic(mlflow_context):
-    exp1, exp2, run1, run2 = _init_exp_test(mlflow_context,
-        ExperimentExporter(mlflow_context.client_src),
-        ExperimentImporter(mlflow_context.client_dst), 
-        verbose=True)
+    exp1, exp2, run1, run2 = _init_exp_test(mlflow_context)
     _compare_experiments(exp1, exp2)
-    compare_runs(mlflow_context.client_src, mlflow_context.client_dst, run1, run2, 
+    compare_runs(mlflow_context.client_src, mlflow_context.client_dst, 
+        run1, run2, 
         mlflow_context.output_dir)
 
 
 def test_exp_with_source_tags(mlflow_context):
-    exp1, exp2, run1, run2 = _init_exp_test(mlflow_context,
-       ExperimentExporter(mlflow_context.client_src),
-       ExperimentImporter(mlflow_context.client_dst, import_source_tags=True), 
-       verbose=False)
+    exp1, exp2, run1, run2 = _init_exp_test(mlflow_context, import_source_tags=True)
     _compare_experiments(exp1, exp2, True)
-    compare_runs(mlflow_context.client_src, mlflow_context.client_dst, run1, run2, 
+    compare_runs(mlflow_context.client_src, mlflow_context.client_dst, 
+        run1, run2, 
         mlflow_context.output_dir, 
         import_source_tags=True)
