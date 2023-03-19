e2d353793b2e9919ce1fbafeadfd7dad02af5aaa
amesar <amesar@users.noreply.github.co>
2023-03-18 22:11:06 -0400

    Issue #89: tests/databricks: adapted print stmts for logging

--- tests/databricks/test_basic.py
+++ tests/databricks/test_basic.py
@@ -23,24 +23,24 @@ def test_train_model(test_context):
     _run_job(test_context, test_context.tester.run_training_job, "Train model")
 
 
-def _test_export_run(test_context):
+def test_export_run(test_context):
     _run_job(test_context, test_context.tester.run_export_run_job, "Export Run")
     _check_dbfs_dir_after_export(test_context, test_context.tester.output_run_base_dir)
 
 
-def _test_import_run(test_context):
+def test_import_run(test_context):
     _run_job(test_context, test_context.tester.run_import_run_job, "Import Run")
     src_run = mlflow_utils.get_last_run(mlflow_client, test_context.tester.ml_exp_path)
     dst_run = mlflow_utils.get_last_run(mlflow_client, test_context.tester.mk_imported_name(test_context.tester.ml_exp_path+"_run"))
     compare_runs(mlflow_client, mlflow_client, src_run, dst_run, test_context.tester.local_artifacts_compare_dir, _use_source_tags)
 
 
-def _test_export_experiment_job(test_context):
+def test_export_experiment_job(test_context):
     _run_job(test_context, test_context.tester.run_export_experiment_job, "Export Experiment")
     _check_dbfs_dir_after_export(test_context, test_context.tester.output_exp_base_dir)
 
 
-def _test_import_experiment_job(test_context):
+def test_import_experiment_job(test_context):
     _run_job(test_context, test_context.tester.run_import_experiment_job, "Import Experiment")
     exp_name_1 = test_context.tester.ml_exp_path
     exp_name_2 = test_context.tester.mk_imported_name(test_context.tester.ml_exp_path)
@@ -53,12 +53,12 @@ def _test_import_experiment_job(test_context):
     compare_runs(mlflow_client, mlflow_client, runs1[0], runs2[0], _mk_artifact_output(test_context), _use_source_tags)
 
 
-def _test_export_model(test_context):
+def test_export_model(test_context):
     _run_job(test_context, test_context.tester.run_export_model_job, "Export Model")
     _check_dbfs_dir_after_export(test_context, test_context.tester.output_exp_base_dir)
 
 
-def _test_import_model_job(test_context):
+def test_import_model_job(test_context):
     _run_job(test_context, test_context.tester.run_import_model_job, "Import Model")
     model_name_1 = test_context.tester.model_name
     model_name_2 = test_context.tester.mk_imported_name(model_name_1)
