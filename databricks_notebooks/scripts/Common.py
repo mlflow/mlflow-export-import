d077f310c64336f7b97023c1305ecab1e61374a5
amesar <amesar@users.noreply.github.co>
2023-03-26 07:35:18 -0400

    Common notebook configure udpate

--- databricks_notebooks/scripts/Common.py
+++ databricks_notebooks/scripts/Common.py
@@ -6,17 +6,8 @@
 # Create standard .databrickscfg in custom location and specify with $DATABRICKS_CONFIG_FILE
 
 def create_databrick_config_file(databricks_config_file=None):
-    context = dbutils.notebook.entry_point.getDbutils().notebook().getContext()
-    token = context.apiToken().get()
-    host_name = context.tags().get("browserHostName").get()
-    user = context.tags().get("user").get()
-
-    import os
-    if not databricks_config_file:
-        databricks_config_file = os.path.join("/tmp", f".databricks.cfg-{user}")
-    print(f"DATABRICKS_CONFIG_FILE: {databricks_config_file}")
-    os.environ["DATABRICKS_CONFIG_FILE"] = databricks_config_file
-    dbutils.fs.put(f"file:///{databricks_config_file}",f"[DEFAULT]\nhost=https://{host_name}\ntoken = "+token,overwrite=True)
+    print("TODO: configure by user")
+    pass
 
 # COMMAND ----------
 
