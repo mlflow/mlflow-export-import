261070a61b054ab8293c227d973fd7ae7f37aa46
amesar <amesar@users.noreply.github.co>
2023-04-24 01:50:32 -0400

    Issue #104: moved fix for mlflow.artifacts.download_artifacts in import_run.py to its own method in mlflow_utils.py

--- mlflow_export_import/common/mlflow_utils.py
+++ mlflow_export_import/common/mlflow_utils.py
@@ -1,4 +1,5 @@
 import os
+import mlflow
 from mlflow.exceptions import RestException
 from mlflow_export_import.common import MlflowExportImportException
 from mlflow_export_import.common.iterators import SearchModelVersionsIterator
@@ -74,9 +75,34 @@ def create_workspace_dir(dbx_client, workspace_dir):
     _logger.info(f"Creating Databricks workspace directory '{workspace_dir}'")
     dbx_client.post("workspace/mkdirs", { "path": workspace_dir })
 
+# == Download artifact issue
+
+def download_artifacts(client, download_uri, dst_path=None, fix=True):
+    """ 
+    Apparently the tracking_uri argument is not honored for mlflow.artifacts.download_artifacts().
+    It seems that tracking_uri is ignored and the global mlflow.get_tracking_uri() is always used.
+    If the two happen to be the same operation will succeed.
+    If not, it fails.
+    Issue: Merge pull request #104 from mingyu89/fix-download-artifacts
+    """ 
+    if fix:
+        previous_tracking_uri = mlflow.get_tracking_uri()
+        mlflow.set_tracking_uri(client._tracking_client.tracking_uri)
+        local_path = mlflow.artifacts.download_artifacts(
+            artifact_uri = download_uri,
+            dst_path = dst_path,
+        )
+        mlflow.set_tracking_uri(previous_tracking_uri)
+    else:
+        local_path = mlflow.artifacts.download_artifacts(
+            artifact_uri = download_uri,
+            dst_path = dst_path,
+            tracking_uri = client._tracking_client.tracking_uri
+        )
+    return local_path
+
 
 # == Dump exception functions
-
 
 def dump_exception(ex, msg=""):
     from mlflow.exceptions import MlflowException
