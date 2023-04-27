049d143f260ac9ab3590f6925baa157a95ce5bcf
darrell.coles@crowncastle.com <darrell.coles@crowncastle.com>
2023-04-27 18:14:55 +0000

    `count-objects-in-mlflow`: swap out `MlflowClient` functionality
    some of its member utilities do not work properly, while the mlflow-scoped versions do (wtf??)

--- count-objects-in-mlflow.py
+++ count-objects-in-mlflow.py
@@ -1,9 +1,7 @@
 # Databricks notebook source
-from mlflow import MlflowClient
+import mlflow
 import pandas as pd
 import numpy as np
-
-client = MlflowClient()
 
 # COMMAND ----------
 
@@ -28,13 +26,13 @@ def email(exp):
   
 def n_runs(exp):
   try:
-    return len(client.search_runs(exp.experiment_id))
+    return len(mlflow.search_runs(exp.experiment_id, run_view_type=3))
   except:
     0
 
 # COMMAND ----------
 
-data = np.array([(exp_id(exp), n_runs(exp), email(exp)) for exp in client.search_experiments()])
+data = np.array([(exp_id(exp), n_runs(exp), email(exp)) for exp in mlflow.search_experiments(view_type=3)])
 
 df = pd.DataFrame(dict(experiment_id=data[:,0], n_runs=data[:,1], owner_email=data[:,2]))
 df["n_runs"] = df.n_runs.astype(int)
@@ -43,8 +41,4 @@ print("There are", df.shape[0], "total experiments")
 print("There are", df.query("n_runs > 0").shape[0], "experiments with at least one run")
 print("There are", df.query("n_runs == 0").shape[0], "experiments with no runs")
 print("There are", df.n_runs.sum(), "runs")
-print("There are", len(client.search_registered_models()), "registered models")
-
-# COMMAND ----------
-
-df
+print("There are", len(mlflow.search_registered_models()), "registered models")
