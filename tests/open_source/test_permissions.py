530f0fed351d3e1ce72d92d4a218e9a6a125a9c6
amesar <amesar@users.noreply.github.co>
2023-05-01 00:29:51 -0400

    Issue #69: added test_permissions.py

---
+++ tests/open_source/test_permissions.py
@@ -0,0 +1,115 @@
+from mlflow_export_import.common.permissions_utils import _map_acl_element, map_acl
+
+
+group_name_element = {
+  "group_name": "admins",
+  "all_permissions": [
+    {
+      "permission_level": "CAN_MANAGE",
+      "inherited": True,
+      "inherited_from_object": [
+        "/directories/"
+      ]
+    }
+  ]
+}
+
+user_name_element = {
+  "user_name": "k2@karakoram.com",
+  "all_permissions": [
+    {
+      "permission_level": "CAN_EDIT",
+      "inherited": True,
+      "inherited_from_object": [
+        "/directories/"
+      ]
+    }
+  ]
+}
+
+user_name_element_2 = {
+  "user_name": "k2@karakoram.com",
+  "all_permissions": [
+    {
+      "permission_level": "CAN_MANAGE",
+      "inherited": False
+    },
+    {
+      "permission_level": "CAN_MANAGE",
+      "inherited": True,
+      "inherited_from_object": [
+        "/directories/767933989557963"
+      ]
+    }
+  ]
+}
+
+mixed_acl = [ group_name_element, user_name_element ] 
+mixed_acl_2 = [ group_name_element, user_name_element_2 ] 
+
+
+def _test_acl_element_group_name():
+    acl2 = _map_acl_element(group_name_element)
+    assert acl2 == [
+      {
+        "group_name": "admins",
+        "permission_level": "CAN_MANAGE"
+      }
+    ]
+
+def _test_acl_element_user_name():
+    acl2 = _map_acl_element(user_name_element)
+    assert acl2 == [
+      {
+        "user_name": "k2@karakoram.com",
+        "permission_level": "CAN_EDIT"
+      }
+    ]
+
+
+def _test_acl_element_user_name_2():
+    acl2 = _map_acl_element(user_name_element_2)
+    assert acl2 == [
+      {
+        "user_name": "k2@karakoram.com",
+        "permission_level": "CAN_MANAGE"
+      },
+      {
+        "user_name": "k2@karakoram.com",
+        "permission_level": "CAN_MANAGE"
+      }
+    ]
+
+
+def _test_acl_mixed():
+    assert map_acl(mixed_acl) == [
+      {
+        "group_name": "admins",
+        "permission_level": "CAN_MANAGE"
+      },
+      {
+        "user_name": "k2@karakoram.com",
+        "permission_level": "CAN_EDIT"
+      }
+    ]
+
+
+def _test_acl_mixed_2():
+    assert map_acl(mixed_acl_2) == [
+      {
+        "group_name": "admins",
+        "permission_level": "CAN_MANAGE"
+      },
+      {
+        "user_name": "k2@karakoram.com",
+        "permission_level": "CAN_MANAGE"
+      },
+      {
+        "user_name": "k2@karakoram.com",
+        "permission_level": "CAN_MANAGE"
+      }
+    ]
+
+
+def test_empty():
+    assert map_acl({}) == []
