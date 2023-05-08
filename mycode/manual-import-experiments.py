30d86bf3be78e6e11a6a094e1d4bf11a1edc4e54
darrell.coles@crowncastle.com <darrell.coles@crowncastle.com>
2023-05-08 16:06:17 +0000

    created `manual-import-experiments`

---
+++ mycode/manual-import-experiments.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+# DBTITLE 1,install latest pkg from github
+# MAGIC %sh 
+# MAGIC #pip install mlflow-export-import
+# MAGIC pip install git+https:///github.com/mlflow/mlflow-export-import/#egg=mlflow-export-import
+
+# COMMAND ----------
+
+# DBTITLE 1,we can see the s3 mount from %sh :)
+# MAGIC %sh ls /dbfs/mnt/ccidsdatascidatalake/
+
+# COMMAND ----------
+
+# DBTITLE 1,set env vars
+import os 
+from datetime import datetime
+import pytz
+
+cst = pytz.timezone('US/Central')
+now = datetime.now(tz=cst)
+date = now.strftime("%Y%m%d_%H%M")
+ 
+logfile = f"import_experiments_{date}.log"
+os.environ["MLFLOW_EXPORT_IMPORT_LOG_OUTPUT_FILE"] = logfile 
+
+os.environ["MLFLOW_EXPORT_IMPORT_LOG_FORMAT"]="%(threadName)s-%(levelname)s-%(message)s"
+
+os.environ["MLFLOW_TRACKING_URI"]="databricks"
+
+with open("/dbfs/FileStore/shared_uploads/darrell.coles@crowncastle.com/aws_databricks_credentials") as f:
+  os.environ["DATABRICKS_HOST"]  = f.readline().strip("\n")
+  os.environ["DATABRICKS_TOKEN"] = f.readline().strip("\n")
+
+# COMMAND ----------
+
+# DBTITLE 1,verify env vars
+# MAGIC %sh
+# MAGIC echo $MLFLOW_EXPORT_IMPORT_LOG_OUTPUT_FILE
+# MAGIC echo $MLFLOW_EXPORT_IMPORT_LOG_FORMAT
+# MAGIC echo $MLFLOW_TRACKING_URI
+# MAGIC # echo $DATABRICKS_HOST
+# MAGIC # echo $DATABRICKS_TOKEN
+
+# COMMAND ----------
+
+# DBTITLE 0,`export-models` options
+# MAGIC %sh 
+# MAGIC import-experiments --help
+
+# COMMAND ----------
+
+# DBTITLE 1,cli execution
+# MAGIC %sh 
+# MAGIC import-experiments \
+# MAGIC   --input-dir /dbfs/mnt/ccidsdatascidatalake/mlflow-migration-01/experiments \
+# MAGIC   --import-source-tags True \
+# MAGIC   --import-permissions True \
+# MAGIC   --use-threads True
+
+# COMMAND ----------
+
+# MAGIC %sh cat /dbfs/mnt/ccidsdatascidatalake/mlflow-migration-01/experiments/experiments.json
+
+# COMMAND ----------
+
+
