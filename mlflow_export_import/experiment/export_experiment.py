6cf4b0054bd4b0f790a420dd6024ff93ee17435a
amesar <amesar@users.noreply.github.co>
2023-05-11 22:28:59 -0400

    Issue #117: removed unnecessary classes from experiment/ scripts

--- mlflow_export_import/experiment/export_experiment.py
+++ mlflow_export_import/experiment/export_experiment.py
@@ -35,134 +35,102 @@ def export_experiment(
         mlflow_client = None
     ):
     """
-    :param experiment_id_or_name: Experiment ID or name.
-    :param output_dir: Output directory.
-    :param run_ids: List of run IDs to export. If None export then all run IDs.
-    :param export_permissions - Export Databricks permissions.
-    :param run_start_time - Only export runs started after this UTC time (inclusive). Format: YYYY-MM-DD.
-    :param notebook_formats: List of notebook formats to export. Values are SOURCE, HTML, JUPYTER or DBC.
-    :param mlflow_client: MLflow client.
+    :param: experiment_id_or_name: Experiment ID or name.
+    :param: output_dir: Output directory.
+    :param: run_ids: List of run IDs to export. If None export then all run IDs.
+    :param: export_permissions - Export Databricks permissions.
+    :param: run_start_time - Only export runs started after this UTC time (inclusive). Format: YYYY-MM-DD.
+    :param: notebook_formats: List of notebook formats to export. Values are SOURCE, HTML, JUPYTER or DBC.
+    :param: mlflow_client: MLflow client.
     :return: Number of successful and number of failed runs.
     """
-    exporter = ExperimentExporter(
-        mlflow_client = mlflow_client,
-        export_permissions = export_permissions,
-        run_start_time = run_start_time,
-        export_deleted_runs = export_deleted_runs,
-        notebook_formats = notebook_formats
-    )
-    return exporter.export_experiment(
-        experiment_id_or_name = experiment_id_or_name,
-        output_dir = output_dir,
-        run_ids = run_ids
-    )
+    
+    mlflow_client = mlflow_client or mlflow.MlflowClient()
+    dbx_client = DatabricksHttpClient(mlflow_client.tracking_uri)
 
+    run_start_time_str = run_start_time
+    if run_start_time:
+        run_start_time = utc_str_to_millis(run_start_time)
 
-class ExperimentExporter():
-
-    def __init__(self,
-            mlflow_client = None,
-            export_permissions = False,
-            run_start_time = None,
-            export_deleted_runs = False,
-            notebook_formats = None
-        ):
-        self.mlflow_client = mlflow_client or mlflow.MlflowClient()
-        self.dbx_client = DatabricksHttpClient(self.mlflow_client.tracking_uri)
-        self.export_permissions = export_permissions
-        self.notebook_formats = notebook_formats
-        self.export_deleted_runs = export_deleted_runs
-
-        self.run_start_time = run_start_time
-        self.run_start_time_str = run_start_time
+    exp = mlflow_utils.get_experiment(mlflow_client, experiment_id_or_name)
+    msg = { "name": exp.name, "id": exp.experiment_id,
+        "mlflow.experimentType": exp.tags.get("mlflow.experimentType", None),
+        "lifecycle_stage": exp.lifecycle_stage
+    } 
+    _logger.info(f"Exporting experiment: {msg}")
+    ok_run_ids = []
+    failed_run_ids = []
+    num_runs_exported = 0
+    if run_ids:
+        for j,run_id in enumerate(run_ids):
+            run = mlflow_client.get_run(run_id)
+            _export_run(mlflow_client, run, output_dir, ok_run_ids, failed_run_ids,
+                run_start_time, run_start_time_str, export_deleted_runs, notebook_formats)
+            num_runs_exported += 1
+    else:
+        kwargs = {}
         if run_start_time:
-            self.run_start_time = utc_str_to_millis(self.run_start_time)
+            kwargs["filter"] = f"start_time > {run_start_time}" 
+        if export_deleted_runs:
+            from mlflow.entities import ViewType
+            kwargs["view_type"] = ViewType.ALL
+        for j,run in enumerate(SearchRunsIterator(mlflow_client, exp.experiment_id, **kwargs)):
+            _export_run(mlflow_client, run, output_dir, ok_run_ids, failed_run_ids,
+                run_start_time, run_start_time_str, export_deleted_runs, notebook_formats)
+            num_runs_exported += 1
+
+    info_attr = {
+        "num_total_runs": (num_runs_exported),
+        "num_ok_runs": len(ok_run_ids),
+        "num_failed_runs": len(failed_run_ids),
+        "failed_runs": failed_run_ids
+    }
+    exp_dct = utils.strip_underscores(exp) 
+    exp_dct["_creation_time"] = fmt_ts_millis(exp.creation_time)
+    exp_dct["_last_update_time"] = fmt_ts_millis(exp.last_update_time)
+    exp_dct["tags"] = dict(sorted(exp_dct["tags"].items()))
+
+    mlflow_attr = { "experiment": exp_dct , "runs": ok_run_ids }
+    if export_permissions:
+        mlflow_attr["permissions"] = permissions_utils.get_experiment_permissions(dbx_client, exp.experiment_id)
+    io_utils.write_export_file(output_dir, "experiment.json", __file__, mlflow_attr, info_attr)
+
+    msg = f"for experiment '{exp.name}' (ID: {exp.experiment_id})"
+    if num_runs_exported==0:
+        _logger.warning(f"No runs exported {msg}")
+    elif len(failed_run_ids) == 0:
+        _logger.info(f"{len(ok_run_ids)} runs succesfully exported {msg}")
+    else:
+        _logger.info(f"{len(ok_run_ids)}/{j} runs succesfully exported {msg}")
+        _logger.info(f"{len(failed_run_ids)}/{j} runs failed {msg}")
+    return len(ok_run_ids), len(failed_run_ids) 
 
 
-    def export_experiment(self,
-            experiment_id_or_name,
-            output_dir,
-            run_ids = None
-        ):
-        """
-        :param experiment_id_or_name: Experiment ID or name.
-        :param output_dir: Output directory.
-        :param run_ids: List of run IDs to export. If None export all run IDs.
-        :return: Number of successful and number of failed runs.
-        """
-        exp = mlflow_utils.get_experiment(self.mlflow_client, experiment_id_or_name)
-        msg = { "name": exp.name, "id": exp.experiment_id,
-            "mlflow.experimentType": exp.tags.get("mlflow.experimentType", None),
-            "lifecycle_stage": exp.lifecycle_stage
-        } 
-        _logger.info(f"Exporting experiment: {msg}")
-        ok_run_ids = []
-        failed_run_ids = []
-        num_runs_exported = 0
-        if run_ids:
-            for j,run_id in enumerate(run_ids):
-                run = self.mlflow_client.get_run(run_id)
-                self._export_run(run, output_dir, ok_run_ids, failed_run_ids)
-                num_runs_exported += 1
-        else:
-            kwargs = {}
-            if self.run_start_time:
-                kwargs["filter"] = f"start_time > {self.run_start_time}" 
-            if self.export_deleted_runs:
-                from mlflow.entities import ViewType
-                kwargs["view_type"] = ViewType.ALL
-            for j,run in enumerate(SearchRunsIterator(self.mlflow_client, exp.experiment_id, **kwargs)):
-                self._export_run(run, output_dir, ok_run_ids, failed_run_ids)
-                num_runs_exported += 1
-
-        info_attr = {
-            "num_total_runs": (num_runs_exported),
-            "num_ok_runs": len(ok_run_ids),
-            "num_failed_runs": len(failed_run_ids),
-            "failed_runs": failed_run_ids
+def _export_run(mlflow_client, run, output_dir, ok_run_ids, failed_run_ids,
+        run_start_time, run_start_time_str,
+        export_deleted_runs, notebook_formats
+    ):
+    if run_start_time and run.info.start_time < run_start_time:
+        msg = { 
+            "run_id": {run.info.run_id}, 
+            "experiment_id": {run.info.experiment_id},
+            "start_time": fmt_ts_millis(run.info.start_time),
+            "run_start_time": run_start_time_str 
         }
-        exp_dct = utils.strip_underscores(exp) 
-        exp_dct["_creation_time"] = fmt_ts_millis(exp.creation_time)
-        exp_dct["_last_update_time"] = fmt_ts_millis(exp.last_update_time)
-        exp_dct["tags"] = dict(sorted(exp_dct["tags"].items()))
+        _logger.info(f"Not exporting run: {msg}")
+        return
 
-        mlflow_attr = { "experiment": exp_dct , "runs": ok_run_ids }
-        if self.export_permissions:
-            mlflow_attr["permissions"] = permissions_utils.get_experiment_permissions(self.dbx_client, exp.experiment_id)
-        io_utils.write_export_file(output_dir, "experiment.json", __file__, mlflow_attr, info_attr)
-
-        msg = f"for experiment '{exp.name}' (ID: {exp.experiment_id})"
-        if num_runs_exported==0:
-            _logger.warning(f"No runs exported {msg}")
-        elif len(failed_run_ids) == 0:
-            _logger.info(f"{len(ok_run_ids)} runs succesfully exported {msg}")
-        else:
-            _logger.info(f"{len(ok_run_ids)}/{j} runs succesfully exported {msg}")
-            _logger.info(f"{len(failed_run_ids)}/{j} runs failed {msg}")
-        return len(ok_run_ids), len(failed_run_ids) 
-
-
-    def _export_run(self, run, output_dir, ok_run_ids, failed_run_ids):
-        if self.run_start_time and run.info.start_time < self.run_start_time:
-            msg = { "run_id": {run.info.run_id}, 
-                "experiment_id": {run.info.experiment_id},
-                "start_time": fmt_ts_millis(run.info.start_time),
-                "run_start_time": self.run_start_time_str }
-            _logger.info(f"Not exporting run: {msg}")
-            return
-
-        run_dir = os.path.join(output_dir, run.info.run_id)
-        is_success = export_run(
-            run_id = run.info.run_id,
-            output_dir = run_dir,
-            export_deleted_runs = self.export_deleted_runs,
-            notebook_formats = self.notebook_formats,
-            mlflow_client = self.mlflow_client
-        )
-        if is_success:
-            ok_run_ids.append(run.info.run_id)
-        else:
-            failed_run_ids.append(run.info.run_id)
+    is_success = export_run(
+        run_id = run.info.run_id,
+        output_dir = os.path.join(output_dir, run.info.run_id),
+        export_deleted_runs = export_deleted_runs,
+        notebook_formats = notebook_formats,
+        mlflow_client = mlflow_client
+    )
+    if is_success:
+        ok_run_ids.append(run.info.run_id)
+    else:
+        failed_run_ids.append(run.info.run_id)
 
 
 @click.command()
--- mlflow_export_import/experiment/import_experiment.py
+++ mlflow_export_import/experiment/import_experiment.py
@@ -16,7 +16,7 @@ from mlflow_export_import.common.click_options import (
 )
 from mlflow_export_import.client.http_client import DatabricksHttpClient
 from mlflow_export_import.common import utils, mlflow_utils, io_utils
-from mlflow_export_import.common.permissions_utils import import_permissions
+from mlflow_export_import.common import permissions_utils
 from mlflow_export_import.common.source_tags import (
     set_source_tags_for_field,
     mk_source_tags_mlflow_tag,
@@ -39,104 +39,64 @@ def import_experiment(
     """
     :param: experiment_name: Destination experiment name.
     :param: input_dir: Source experiment directory.
-    :param import_permissions: Import Databricks permissions.
-    :param import_source_tags: Import source information for MLFlow objects and create tags in destination object.
-    :param use_src_user_id: Set the destination user ID to the source user ID.
-                            Source user ID is ignored when importing into
-    :param mlflow_client: MLflow client.
+    :param: import_source_tags: Import source information for MLflow objects and create tags in destination object.
+    :param: import_permissions: Import Databricks permissions.
+    :param: use_src_user_id: Set the destination user ID to the source user ID.
+                            Source user ID is ignored when importing into Databricks.
+    :param: dst_notebook_dir: Destination Databricks workspace directory if importing notebook.
+    :param: mlflow_client: MLflow client.
     :return: A map of source run IDs and destination run.info.
     """
-    importer = ExperimentImporter(
-        import_source_tags = import_source_tags,
-        import_permissions = import_permissions,
-        use_src_user_id = use_src_user_id,
-        mlflow_client = mlflow_client
-    )
-    return importer.import_experiment(
-        experiment_name = experiment_name,
-        input_dir = input_dir,
-        dst_notebook_dir = dst_notebook_dir
-    )
 
+    mlflow_client = mlflow_client or mlflow.MlflowClient()
+    dbx_client = DatabricksHttpClient(mlflow_client.tracking_uri)
 
-class ExperimentImporter():
+    path = io_utils.mk_manifest_json_path(input_dir, "experiment.json")
+    root_dct = io_utils.read_file(path)
+    info = io_utils.get_info(root_dct)
+    mlflow_dct = io_utils.get_mlflow(root_dct)
+    exp_dct = mlflow_dct["experiment"]
 
-    def __init__(self,
-            import_source_tags = False,
-            import_permissions = False,
-            use_src_user_id = False,
-            mlflow_client = None
-        ):
-        """
-        :param mlflow_client: MLflow client.
-        :param import_source_tags: Import source information for MLFlow objects and create tags in destination object.
-        :param use_src_user_id: Set the destination user ID to the source user ID.
-                                Source user ID is ignored when importing into
-        """
-        self.mlflow_client = mlflow_client or mlflow.MlflowClient()
-        self.dbx_client = DatabricksHttpClient(self.mlflow_client.tracking_uri)
-        self.import_source_tags = import_source_tags
-        self.use_src_user_id = use_src_user_id
-        self.import_permissions = import_permissions
+    tags = exp_dct["tags"] 
+    if import_source_tags:
+        source_tags = mk_source_tags_mlflow_tag(tags)
+        tags = { **tags, **source_tags }
+        exp = mlflow_dct["experiment"]
+        set_source_tags_for_field(exp, tags)
+        fmt_timestamps("creation_time", exp, tags)
+        fmt_timestamps("last_update_time", exp, tags)
 
+    exp = mlflow_utils.set_experiment(mlflow_client, dbx_client, experiment_name, tags)
 
-    def import_experiment(self,
-            experiment_name,
-            input_dir,
-            dst_notebook_dir = None
-        ):
-        """
-        :param: experiment_name: Destination experiment name.
-        :param: input_dir: Source experiment directory.
-        :return: A map of source run IDs and destination run.info.
-        """
+    if import_permissions:
+        perms_dct = mlflow_dct.get("permissions", None)
+        if perms_dct:
+            permissions_utils.import_permissions(dbx_client, perms_dct, "experiment", exp.name, exp.experiment_id)
 
-        path = io_utils.mk_manifest_json_path(input_dir, "experiment.json")
-        root_dct = io_utils.read_file(path)
-        info = io_utils.get_info(root_dct)
-        mlflow_dct = io_utils.get_mlflow(root_dct)
-        exp_dct = mlflow_dct["experiment"]
+    run_ids = mlflow_dct["runs"]
+    failed_run_ids = info["failed_runs"]
 
-        tags = exp_dct["tags"] 
-        if self.import_source_tags:
-            source_tags = mk_source_tags_mlflow_tag(tags)
-            tags = { **tags, **source_tags }
-            exp = mlflow_dct["experiment"]
-            set_source_tags_for_field(exp, tags)
-            fmt_timestamps("creation_time", exp, tags)
-            fmt_timestamps("last_update_time", exp, tags)
+    _logger.info(f"Importing {len(run_ids)} runs into experiment '{experiment_name}' from '{input_dir}'")
+    run_ids_map = {}
+    run_info_map = {}
+    for src_run_id in run_ids:
+        dst_run, src_parent_run_id = import_run(
+            mlflow_client = mlflow_client,
+            experiment_name = experiment_name,
+            input_dir = os.path.join(input_dir, src_run_id),
+            dst_notebook_dir = dst_notebook_dir,
+            import_source_tags = import_source_tags,
+            use_src_user_id = use_src_user_id
+        )
+        dst_run_id = dst_run.info.run_id
+        run_ids_map[src_run_id] = { "dst_run_id": dst_run_id, "src_parent_run_id": src_parent_run_id }
+        run_info_map[src_run_id] = dst_run.info
+    _logger.info(f"Imported {len(run_ids)} runs into experiment '{experiment_name}' from '{input_dir}'")
+    if len(failed_run_ids) > 0:
+        _logger.warning(f"{len(failed_run_ids)} failed runs were not imported - see '{path}'")
+    utils.nested_tags(mlflow_client, run_ids_map)
 
-        exp = mlflow_utils.set_experiment(self.mlflow_client, self.dbx_client, experiment_name, tags)
-
-        if self.import_permissions:
-            perms_dct = mlflow_dct.get("permissions", None)
-            if perms_dct:
-                import_permissions(self.dbx_client, perms_dct, "experiment", exp.name, exp.experiment_id)
-
-        run_ids = mlflow_dct["runs"]
-        failed_run_ids = info["failed_runs"]
-
-        _logger.info(f"Importing {len(run_ids)} runs into experiment '{experiment_name}' from '{input_dir}'")
-        run_ids_map = {}
-        run_info_map = {}
-        for src_run_id in run_ids:
-            dst_run, src_parent_run_id = import_run(
-                mlflow_client = self.mlflow_client,
-                experiment_name = experiment_name,
-                input_dir = os.path.join(input_dir, src_run_id),
-                dst_notebook_dir = dst_notebook_dir,
-                import_source_tags = self.import_source_tags,
-                use_src_user_id = self.use_src_user_id
-            )
-            dst_run_id = dst_run.info.run_id
-            run_ids_map[src_run_id] = { "dst_run_id": dst_run_id, "src_parent_run_id": src_parent_run_id }
-            run_info_map[src_run_id] = dst_run.info
-        _logger.info(f"Imported {len(run_ids)} runs into experiment '{experiment_name}' from '{input_dir}'")
-        if len(failed_run_ids) > 0:
-            _logger.warning(f"{len(failed_run_ids)} failed runs were not imported - see '{path}'")
-        utils.nested_tags(self.mlflow_client, run_ids_map)
-
-        return run_info_map
+    return run_info_map
 
 
 @click.command()
