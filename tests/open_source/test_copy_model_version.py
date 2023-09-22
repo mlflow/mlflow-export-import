51a13ab8b0db387b14eb343d2b8306b993f861ef
amesar <amesar@users.noreply.github.co>
2023-09-21 20:02:15 -0400

    Issue #138 and Issue #141: adapted tests to optional src-mlflow-uri and dst-mlflow-uri parameters

--- tests/open_source/test_copy_model_version.py
+++ tests/open_source/test_copy_model_version.py
@@ -1,12 +1,13 @@
+import os
 from mlflow_export_import.common.source_tags import ExportTags
 from mlflow_export_import.copy import local_utils
 from mlflow_export_import.copy import copy_model_version
 
-from tests.open_source.init_tests import mlflow_context
-from tests.open_source.oss_utils_test import create_experiment
-from tests.open_source.oss_utils_test import mk_test_object_name_default
-from tests.open_source.oss_utils_test import create_version
 from tests.compare_utils import compare_runs
+from . init_tests import mlflow_context
+from . init_tests import MlflowContext
+from . oss_utils_test import mk_test_object_name_default
+from . oss_utils_test import create_experiment, create_version
 
 
 def test_with_experiment(mlflow_context):
@@ -47,9 +48,35 @@ def test_without_experiment(mlflow_context):
     )
     assert vr == src_vr
     _compare_model_versions(src_vr, dst_vr)
-
     assert src_vr.run_id == dst_vr.run_id
     assert dst_vr == mlflow_context.client_src.get_model_version(dst_vr.name, dst_vr.version)
+
+
+def test_without_dst_tracking_uri(mlflow_context):
+    dst_exp = create_experiment(mlflow_context.client_src)
+    vr, _ = _create_model_version(mlflow_context)
+    dst_model_name = mk_test_object_name_default()
+        
+    src_vr, dst_vr = copy_model_version.copy(
+        src_model_name = vr.name,
+        src_model_version = vr.version,
+        dst_model_name = dst_model_name,
+        dst_experiment_name = dst_exp.name,
+        #src_tracking_uri = mlflow_context.client_src.tracking_uri,
+        #dst_tracking_uri = mlflow_context.client_dst.tracking_uri,
+        verbose = False
+    )
+    mlflow_context = MlflowContext(
+        mlflow_context.client_src, 
+        mlflow_context.client_src, 
+        mlflow_context.output_dir, 
+        os.path.join(mlflow_context.output_dir,"run")
+    )
+    assert vr == src_vr
+    _compare_model_versions(src_vr, dst_vr)
+    assert src_vr.run_id != dst_vr.run_id
+    _compare_runs(mlflow_context, src_vr, dst_vr)
+    assert dst_vr == mlflow_context.client_dst.get_model_version(dst_vr.name, dst_vr.version)
 
 
 def test_with_experiment_and_copy_tags(mlflow_context):
--- tests/open_source/test_copy_run.py
+++ tests/open_source/test_copy_run.py
@@ -8,25 +8,27 @@ from tests.open_source.oss_utils_test import mk_test_object_name_default
 
 # == Setup
 
-def _init_run_test(mlflow_context, run_name = None, model_artifact = "model"):
-    _, src_run = create_simple_run(mlflow_context.client_src,
-        run_name = run_name,
-        model_artifact = model_artifact
-    )
+def _init_run_test(mlflow_context, dst_mlflow_uri=None):
+    _, src_run = create_simple_run(mlflow_context.client_src, model_artifact = "model")
     dst_exp_name = mk_test_object_name_default()
     dst_run = copy_run.copy(
         src_run.info.run_id, 
         dst_exp_name, 
         mlflow_context.client_src.tracking_uri,
-        mlflow_context.client_dst.tracking_uri
+        dst_mlflow_uri 
     )
     return src_run, dst_run
 
 
 # == Regular tests
     
-def test_run_basic(mlflow_context): 
-    run1, run2 = _init_run_test(mlflow_context, "test_run_basic")
+def test_run_basic_without_dst_mlflow_uri(mlflow_context): 
+    run1, run2 = _init_run_test(mlflow_context)
+    compare_runs(mlflow_context, run1, run2)
+
+
+def test_run_basic_with_dst_mlflow_uri(mlflow_context): 
+    run1, run2 = _init_run_test(mlflow_context, mlflow_context.client_dst.tracking_uri)
     compare_runs(mlflow_context, run1, run2)
 
 
