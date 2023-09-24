b6b1099aa39d32cc4e3e9e27a980ec45acae4bbf
amesar <amesar@users.noreply.github.co>
2023-09-24 16:05:48 -0400

    Issue #111: fixed create_run in local_utils.py

--- tests/databricks/local_utils.py
+++ tests/databricks/local_utils.py
@@ -1,5 +1,6 @@
+import mlflow
 from tests.open_source.oss_utils_test import mk_test_object_name_default
-from . import sklearn_utils
+from tests.open_source import sklearn_utils
 from . init_tests import workspace_src
 
 
@@ -7,28 +8,24 @@ def _mk_experiment_name(workspace=workspace_src):
     return f"{workspace.base_dir}/{mk_test_object_name_default()}"
 
 
-def create_experiment(mlflow_client):
-    exp_id = mlflow_client.create_experiment(_mk_experiment_name(), tags={"ocean": "southern"})
-    return mlflow_client.get_experiment(exp_id)
+def create_experiment(client):
+    exp_id = client.create_experiment(_mk_experiment_name(), tags={"ocean": "southern"})
+    return client.get_experiment(exp_id)
 
 
-def create_run(mlflow_client, experiment_id):
+def create_run(client, experiment_id):
     max_depth = 4
-    run = mlflow_client.create_run(experiment_id)
     model = sklearn_utils.create_sklearn_model(max_depth)
-
-    model_path = "model.pkl"
-    _write_model(model, model_path)
-    mlflow_client.log_artifact(run.info.run_id, model_path, "model")
-
-    mlflow_client.log_param(run.info.run_id, "max_depth",max_depth)
-    mlflow_client.log_metric(run.info.run_id, "rmse",0.789)
-
-    mlflow_client.set_terminated(run.info.run_id)
-    return mlflow_client.get_run(run.info.run_id)
-
-
-def _write_model(model, path):
-    import cloudpickle as pickle
-    with open(path,"wb") as f:
-        pickle.dump(model, f)
+    ori_tracking_uri = mlflow.tracking.get_tracking_uri()
+    mlflow.set_tracking_uri(client.tracking_uri)
+    with mlflow.start_run(experiment_id=experiment_id) as run:
+        mlflow.log_param("max_depth",max_depth)
+        mlflow.log_metric("rmse", 0.789)
+        mlflow.set_tag("my_tag", "my_val")
+        mlflow.sklearn.log_model(model, "model")
+        mlflow.set_tag("south_america", "aconcagua")
+        with open("info.txt", "w", encoding="utf-8") as f:
+            f.write("Hi artifact")
+        mlflow.log_artifact("info.txt")
+    mlflow.set_tracking_uri(ori_tracking_uri)
+    return client.get_run(run.info.run_id)
