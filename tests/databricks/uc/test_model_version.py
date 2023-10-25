0f10d1ca268edae77b897b82340e54851f4245dc
amesar <amesar@users.noreply.github.co>
2023-10-24 22:57:07 -0400

    Issue #25: added tests for UC test_model_version.py

---
+++ tests/databricks/uc/test_model_version.py
@@ -0,0 +1,8 @@
+from tests.databricks.init_tests import test_context
+from tests.databricks import _test_model_version
+
+def test_import_metadata_false(test_context):
+    _test_model_version.test_import_metadata_false(test_context, True)
+
+def test_import_metadata_true(test_context):
+    _test_model_version.test_import_metadata_true(test_context, True)
