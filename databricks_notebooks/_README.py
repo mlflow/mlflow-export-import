a4e79486412ca7cb741c4ab934eaf1ff80eb8d33
amesar <amesar>
2024-02-05 14:38:00 +0000

    Updated Databricks notebook _README

--- databricks_notebooks/_README.py
+++ databricks_notebooks/_README.py
@@ -2,22 +2,23 @@
 # MAGIC %md ### MLflow Export Import - Databricks Notebooks
 # MAGIC
 # MAGIC #### Overview
-# MAGIC * Copy MLflow objects (runs, experiments or registered models) between MLflow workspaces (tracking servers).
+# MAGIC * Copy MLflow objects (runs, experiments or registered models) either in the same workspace or to another workspace.
+# MAGIC * Supports both classical Workspace Registry and newer Unity Catalog Registry.
 # MAGIC * Customers often need to copy MLflow objects to another workspace.
 # MAGIC   * For example, we train model runs in the dev workspace, and then we wish to promote the best run to a prod workspace.
 # MAGIC   * No out-of-the-box way to do this today.
 # MAGIC   * Customer MLflow object data is currently locked into a workspace and not portable.
-# MAGIC * In order to copy MLflow objects between workspaces, you will need to either:
-# MAGIC   * Set up a shared cloud object location mounted on each workspace's DBFS.
-# MAGIC   * Or use a volume shared by both workspaces using the same Unity Catalog metastore.
+# MAGIC * In order to copy MLflow objects between workspaces, you need to use a shared cloud storage location:
+# MAGIC   * DBFS: Create a [DBFS mount point](https://docs.databricks.com/en/dbfs/mounts.html) on both workspaces pointing to the shared location.
+# MAGIC     * Example: `dbfs:/mnt/mlflow_export_import/single/experiments`
+# MAGIC   * Unity Catalog: use a volume shared by both workspaces using the same Unity Catalog metastore.
+# MAGIC     * Example: `/Volumes/my_catalog/my_schema/mlflow_export_import_volume/single/experiments`
 # MAGIC
 # MAGIC #### Details
 # MAGIC
-# MAGIC * [MLflow Export Import](https://databricks.atlassian.net/wiki/spaces/UN/pages/800754006/MLflow+Export+Import) - Internal Databricks wiki page.
-# MAGIC * Github:
-# MAGIC   * [README](https://github.com/mlflow/mlflow-export-import/blob/master/README.md)
-# MAGIC   * Source code: https://github.com/mlflow/mlflow-export-import - source of truth with extensive documentation.
-# MAGIC   * Databricks notebooks: https://github.com/mlflow/mlflow-export-import/tree/master/databricks_notebooks.
+# MAGIC * [README](https://github.com/mlflow/mlflow-export-import/blob/master/README.md)
+# MAGIC * Source code: https://github.com/mlflow/mlflow-export-import - source of truth with extensive documentation.
+# MAGIC * Databricks notebooks: https://github.com/mlflow/mlflow-export-import/tree/master/databricks_notebooks.
 # MAGIC   
 # MAGIC #### Architecture
 # MAGIC
@@ -25,35 +26,24 @@
 # MAGIC
 # MAGIC #### Notebooks 
 # MAGIC
-# MAGIC ##### Basic notebooks
+# MAGIC ##### Notebooks
 # MAGIC * [Single notebooks]($single/_README) - Copy one MLflow object from one tracking server (workspace) to another.
 # MAGIC * [Bulk notebooks]($bulk/_README) - Copy multiple MLflow objects. The target object name will be the same as the source object name.
-# MAGIC * [Copy notebooks]($copy/_README) - Direct copy of an MLflow object with intermediate storage as a temp directory.
-# MAGIC
-# MAGIC ##### [Console Script]($scripts/_README) notebooks 
-# MAGIC * Command-line scripts using the Linux shell (%sh).
-# MAGIC * [Console_Scripts]($scripts/Console_Scripts) 
+# MAGIC * [Copy notebooks]($copy/_README) - Directly copy a model version or run using a temp directory as intermediate.
 # MAGIC
 # MAGIC #### Limitations
 # MAGIC
-# MAGIC * [General limitations](https://github.com/mlflow/mlflow-export-import/blob/master/README_limitations.md#general-limitations).
-# MAGIC * [Databricks limitations](https://github.com/mlflow/mlflow-export-import/blob/master/README_limitations.md#databricks-limitations).
+# MAGIC * [General limitations](https://github.com/mlflow/mlflow-export-import/blob/master/README_limitations.md#general-limitations)
+# MAGIC * [Databricks limitations](https://github.com/mlflow/mlflow-export-import/blob/master/README_limitations.md#databricks-limitations)
 # MAGIC
-# MAGIC #### Setup
+# MAGIC ##### [Console Script]($scripts/_README) notebooks - experimental
+# MAGIC * Command-line scripts using the Linux shell (%sh).
+# MAGIC * [Console_Scripts]($scripts/Console_Scripts) 
 # MAGIC
-# MAGIC Use [notebook scoped libraries](https://docs.databricks.com/libraries/notebooks-python-libraries.html#install-notebook-scoped-libraries-with-pip) to install the [mlflow-export-import](https://pypi.org/project/mlflow-export-import) library in your notebook.
+# MAGIC #### Databricks internal
+# MAGIC * [MLflow Export Import](https://databricks.atlassian.net/wiki/spaces/UN/pages/800754006/MLflow+Export+Import) - Wiki page.
 # MAGIC
-# MAGIC **Install from PyPI**
-# MAGIC ```
-# MAGIC pip install mlflow-export-import
-# MAGIC ```
-# MAGIC
-# MAGIC **Install from github**
-# MAGIC
-# MAGIC ```
-# MAGIC pip install git+https:///github.com/amesar/mlflow-export-import/#egg=mlflow-export-import
-# MAGIC ```
 
 # COMMAND ----------
 
-# MAGIC %md ##### Last updated: _2024-01-23_
+# MAGIC %md ##### Last updated: _2024-02-05_
