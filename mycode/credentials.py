f226fae265bca51e4ddde89f929c1d292591a397
dcoles--cc <dcoles--cc>
2025-02-19 17:04:15 +0000

    create get_credentials_path()
    get credential path based on platform

---
+++ mycode/credentials.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+def get_credentials_path(platform):
+  
+  az_creds_path = "/dbfs/FileStore/shared_uploads/darrell.coles@crowncastle.com/azure_databricks_credentials"
+  aws_creds_path = "/dbfs/FileStore/tables/aws_databricks_credentials"
+
+  match platform:
+    case "": raise ValueError("platform must be specified")
+    case "azure": return az_creds_path
+    case "aws": return aws_creds_path
