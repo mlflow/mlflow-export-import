e20d35d96bb3184254fdc5f210a33ce64dfecffa
dcoles--cc <dcoles--cc>
2025-02-24 22:56:23 +0000

    update get-model-hashes to operate on both platforms

--- mycode/get-model-hashes.py
+++ mycode/get-model-hashes.py
@@ -1,12 +1,18 @@
 # Databricks notebook source
-# MAGIC %config Completer.use_jedi=False
+# DBTITLE 1,Libs
+import mlflow
 
 # COMMAND ----------
 
-import mlflow
+# DBTITLE 1,variables
+dbutils.widgets.dropdown("platform", "", ["", "azure", "aws"])
+platform = dbutils.widgets.get("platform")
 
+# COMMAND ----------
+
+# DBTITLE 1,execute
 for model in mlflow.search_registered_models():
-  result = dbutils.notebook.run("get-model-hash", -1, {"registered-model-name": model.name})
+  result = dbutils.notebook.run("get-model-hash", -1, {"registered-model-name": model.name, "platform": platform})
   print(result)
 
 # COMMAND ----------
